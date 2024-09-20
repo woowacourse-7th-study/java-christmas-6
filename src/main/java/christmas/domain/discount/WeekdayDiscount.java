00645187832e6bc2aff896398f4afddc1f979ca6
JHZLO <010703kjh@gamil.com>
2024-09-21 03:50:04 +0900

    feat(weekday-discount): 평일 할인 객체 생성

--- src/main/java/christmas/domain/discount/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -4,9 +4,38 @@ import static christmas.constants.Number.EVENT_MONTH;
 import static christmas.constants.Number.EVENT_YEAR;
 
 import christmas.domain.Orders;
-import christmas.dto.VisitDateDto;
+import christmas.domain.vo.MenuType;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-public class WeekdayDiscount {
+public class WeekdayDiscount implements Discount {
+
+    private static final int WEEKDAY_DISCOUNT_PRICE = 2023;
+    private static final String WEEKDAY_DISCOUNT_STRING = "평일 할인";
+
+    @Override
+    public int applyDiscount(Orders orders) {
+        int orderDay = orders.getDate();
+        if (isWeekend(orderDay)) {
+            return 0;
+        }
+        return getDiscountPrice(orders);
+    }
+
+    @Override
+    public String getDiscountName() {
+        return WEEKDAY_DISCOUNT_STRING;
+    }
+
+    public boolean isWeekend(int orderDay) {
+        LocalDate date = LocalDate.of(EVENT_YEAR, EVENT_MONTH, orderDay);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        return (dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY);
+    }
+
+    private int getDiscountPrice(Orders orders) {
+        int desertMenuCount = orders.findCountMenuType(MenuType.DESSERT);
+        return desertMenuCount * WEEKDAY_DISCOUNT_PRICE;
+    }
 }
