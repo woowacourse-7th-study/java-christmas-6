8b35bba4f86c70ebc0e87615c87765227105dec0
JHZLO <010703kjh@gamil.com>
2024-09-21 03:54:30 +0900

    feat(weekend-discount): 주말 할인 객체 생성

--- src/main/java/christmas/domain/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/discount/WeekendDiscount.java
@@ -4,26 +4,28 @@ import static christmas.constants.Number.EVENT_MONTH;
 import static christmas.constants.Number.EVENT_YEAR;
 
 import christmas.domain.Orders;
+import christmas.domain.discount.Discount;
 import christmas.domain.vo.MenuType;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
-public class WeekdayDiscount implements Discount {
-    private static final int WEEKDAY_DISCOUNT_PRICE = 2023;
-    private static final String WEEKDAY_DISCOUNT_STRING = "평일 할인";
+public class WeekendDiscount implements Discount {
+
+    private static final int WEEKEND_DISCOUNT_PRICE = 2023;
+    private static final String WEEKEND_DISCOUNT_STRING = "주말 할인";
 
     @Override
     public int applyDiscount(Orders orders) {
         int orderDay = orders.getDate();
         if (isWeekend(orderDay)) {
-            return 0;
+            return getDiscountPrice(orders);
         }
-        return getDiscountPrice(orders);
+        return 0;
     }
 
     @Override
     public String getDiscountName() {
-        return WEEKDAY_DISCOUNT_STRING;
+        return WEEKEND_DISCOUNT_STRING;
     }
 
     public boolean isWeekend(int orderDay) {
@@ -34,7 +36,7 @@ public class WeekdayDiscount implements Discount {
     }
 
     private int getDiscountPrice(Orders orders) {
-        int desertMenuCount = orders.findCountMenuType(MenuType.DESSERT);
-        return desertMenuCount * WEEKDAY_DISCOUNT_PRICE;
+        int mainMenuCount = orders.findCountMenuType(MenuType.MAIN);
+        return mainMenuCount * WEEKEND_DISCOUNT_PRICE;
     }
 }
