03003890209ea7ff481b6d31c3901f396fa995d1
JHZLO <010703kjh@gamil.com>
2024-09-21 04:00:17 +0900

    feat(gift): Gift 객체 Discount에 상속 받도록 구성

--- src/main/java/christmas/domain/discount/Gift.java
+++ src/main/java/christmas/domain/discount/Gift.java
@@ -1,15 +1,30 @@
 package christmas.domain.discount;
 
 import christmas.domain.OrderProduct;
+import christmas.domain.Orders;
 import christmas.domain.vo.Product;
 import org.junit.jupiter.api.Order;
 
-public class Gift {
+public class Gift implements Discount {
     private static final String DEFAULT_GIFT_QUANTITY = "1개";
+    private static final String GIFT_DISCOUNT_STRING = "증정 이벤트";
     private final boolean isGiftAvailable;
 
     public Gift(boolean isGiftAvailable) {
         this.isGiftAvailable = isGiftAvailable;
+    }
+
+    @Override
+    public int applyDiscount(Orders orders) {
+        if (isGiftAvailable) {
+            return Product.CHAMPAGNE.getPrice();
+        }
+        return 0;
+    }
+
+    @Override
+    public String getDiscountName() {
+        return GIFT_DISCOUNT_STRING;
     }
 
     public String getGiftMessage() {
