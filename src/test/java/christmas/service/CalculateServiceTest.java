4fba6b33fa6060de54cb860284d5ab3f49f3e397
JHZLO <010703kjh@gamil.com>
2024-09-21 23:46:38 +0900

    test(calculate-service-test): eventBadgeType 테스트 코드 추가 작성

--- src/test/java/christmas/service/CalculateServiceTest.java
+++ src/test/java/christmas/service/CalculateServiceTest.java
@@ -1,7 +1,9 @@
 package christmas.service;
 
 import christmas.domain.Orders;
+import christmas.domain.vo.BadgeType;
 import christmas.dto.BenefitDto;
+import christmas.dto.DiscountDto;
 import christmas.dto.OrdersDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -46,5 +48,51 @@ class CalculateServiceTest {
 
         // then
         assertThat(benefitDto.getGiftMessage()).isEqualTo("없음");
+    }
+    @Test
+    @DisplayName("할인 금액이 5천원 이상, 1만원 미만인 경우 별 배지가 부여된다.")
+    void returnsStarBadgeFor5000() {
+        // given
+        DiscountDto discountDto = new DiscountDto(null, 7000);
+        // when
+        BadgeType badgeType = calculateService.calculateBadgeType(discountDto);
+
+        // then
+        assertThat(badgeType).isEqualTo(BadgeType.STAR_BADGE);
+    }
+    @Test
+    @DisplayName("할인 금액이 1만원 이상, 2만원 미만일 경우 트리 배지가 부여된다.")
+    void returnsTreeBadgeFor10000() {
+        // given
+        DiscountDto discountDto = new DiscountDto(null, 15000);
+        // when
+        BadgeType badgeType = calculateService.calculateBadgeType(discountDto);
+
+        // then
+        assertThat(badgeType).isEqualTo(BadgeType.TREE_BADGE);
+    }
+
+    @Test
+    @DisplayName("할인 금액이 2만원 이상일 경우 산타 배지가 부여된다.")
+    void returnsSantaBadgeFor20000() {
+        // given
+        DiscountDto discountDto = new DiscountDto(null, 30000); // 총 할인 금액이 20000원
+        // when
+        BadgeType badgeType = calculateService.calculateBadgeType(discountDto);
+
+        // then
+        assertThat(badgeType).isEqualTo(BadgeType.SANTA_BADGE);
+    }
+
+    @Test
+    @DisplayName("할인 금액이 5천원 미만일 경우 배지가 부여되지 않는다.")
+    void returnsNoBadgeForBelow5000() {
+        // given
+        DiscountDto discountDto = new DiscountDto(null, 4000); // 총 할인 금액이 4000원
+        // when
+        BadgeType badgeType = calculateService.calculateBadgeType(discountDto);
+
+        // then
+        assertThat(badgeType).isNull();
     }
 }
