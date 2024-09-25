9c031f7753ab18561d17408e0aa43503a80eb9ce
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-09-25 16:18:56 +0900

    test(visit-day-test): VisitDay 모델 테스트 작성

---
+++ src/test/java/christmas/model/VisitDayTest.java
@@ -0,0 +1,37 @@
+package christmas.model;
+
+import christmas.constants.error.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class VisitDayTest {
+
+    @Test
+    @DisplayName("사용자의 입력을 제대로 받아오는 데 성공한다.")
+    void getVisitDay() {
+        // given
+        int expected = 1;
+
+        // when
+        VisitDay visitDay = new VisitDay(expected);
+        int real = visitDay.getVisitDay();
+
+        // then
+        assertThat(real).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("사용자의 입력이 허용 범위 밖인 경우 예외가 발생한다.")
+    void validateVisitDayRange() {
+        // given
+        int outOfRange = 32;
+
+        // when & then
+        assertThatThrownBy(() -> new VisitDay(outOfRange))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.INVALID_VISIT_DAY);
+    }
+}
